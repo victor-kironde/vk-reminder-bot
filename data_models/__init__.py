607cfd7ae1292b0a2191dbc75570de844e7c8d64
Victor Kironde <victorkironde@gmail.com>
2020-04-09 20:55:45 +0300

    Add ActivityMappingState

--- data_models/__init__.py
+++ data_models/__init__.py
@@ -1,4 +1,5 @@
 from .reminder import Reminder, ReminderLog
 from .welcome_user_state import WelcomeUserState
+from .activity_mapping_state import ActivityMappingState
 
-__all__ = ["Reminder", "ReminderLog", "WelcomeUserState"]
+__all__ = ["Reminder", "ReminderLog", "WelcomeUserState", "ActivityMappingState"]
